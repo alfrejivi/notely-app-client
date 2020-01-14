8cf5c9b653d640b98efae0449961055e69ee80d5
Freddy Jimenez <fjimenez@qwic.nl>
2020-01-14 17:19:52 +0100

    attachment url fixed

--- src/containers/Notes.tsx
+++ src/containers/Notes.tsx
@@ -37,7 +37,7 @@ const Notes: React.FC<NotesProps> = (props) => {
                 const { content, attachment } = note;
 
                 if (attachment) {
-                    note.attachmentURL = await Storage.vault.get(attachment);
+                    note.attachmentURL = await Storage.vault.get(attachment.key);
                 }
 
                 fields.content = content;
